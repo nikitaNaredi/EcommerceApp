7efdf884fd617720634ee2170ebed99c41fe13ec
nikita.kumari <nikita.kumari@avalara.com>
2021-10-02 22:00:14 +0530

    Added not found return

--- Ecommerce.API/Ecommerce.API/Controllers/AccountController.cs
+++ Ecommerce.API/Ecommerce.API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Ecommerce.API.Controllers
@@ -34,9 +35,9 @@ namespace Ecommerce.API.Controllers
             if (result.Succeeded)
             {
                 await signInManager.SignInAsync(user, isPersistent: false);
+                return Ok();
             }
-            return Ok();
-
+            return StatusCode((int)HttpStatusCode.NotFound);
         }
 
         [HttpPost]
@@ -45,6 +46,18 @@ namespace Ecommerce.API.Controllers
         {
             await signInManager.SignOutAsync();
             return Ok();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return StatusCode((int)HttpStatusCode.NotFound);
         }
     }
 }
